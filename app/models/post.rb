6a7562e7d1a3b6ef599e0cbd171a854401d9daa5
winifredf <winifred.dykes@hotmail.com>
2020-06-14 01:31:42 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :validator
+
+    def validator
+        if title != nil && !title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, "clickbaity")
+        end
+    end
+  
+    # validates :title, length: { minimum: 2 }
+    # validates :content, length: { maximum: 500 }
+    # validates :summary, length: { in: 6..20 }
+    # validates :category, length: { is: 6 }
+    # validate :validator
+
+    # def validator
+    #     if title != nil && !title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+    #         errors.add(:title, "clickbaity")
+    #     end
+    # end
+
 end
